7cd98c153a61716a3a0c6e4faceaff45b2c3b09c
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-12-22 21:58:10 -0800

    fix send Token

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,21 +1,16 @@
-// create a token and saving in the cookies
-const sendToken=(user,statusCode,res)=>{
- const token=user.getJwtToken();
-  //options for cookies
-  const options={
-    httpOnly: true,               // fix case
-  expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-  sameSite: process.env.NODE_ENV === "PRODUCTION" ? "none" : "lax",
-  secure: process.env.NODE_ENV === "PRODUCTION", // secure true in prod (https)
-    
-  }
-  res.status(statusCode).cookie("token",token,options).json({
-    message:"successfully user verified",
-    success:true,
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "none", // Isay manual "none" kar dein
+    secure: true,      // Isay manual true kar dein (HTTPS ke liye)
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
     user,
-    token
-  })
-
-}
-
-module.exports=sendToken;
+    token,
+  });
+};
