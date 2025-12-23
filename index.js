6c258416405d1d89f0df8e7ee64d08bd2df506cc
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-12-22 22:44:07 -0800

    fix profile update

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
